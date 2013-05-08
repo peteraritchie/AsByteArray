b24c93eba3f2df92fece58e4231a2dfdc40d6bb0
peteraritchie <github.com@peterritchie.com>
2013-05-08 10:49:52 -0400

    Created project

---
+++ AsByteArray/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AsByteArray
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length < 1)
+			{
+				Usage();
+				return;
+			}
+
+			var fileStream = File.OpenRead(args[0]);
+			using (var reader = new BinaryReader(fileStream))
+			{
+				AsByteArray("bytes", Console.Out, fileStream.Length, reader);
+			}
+
+		}
+
+		private static void Usage()
+		{
+			Console.WriteLine("AsByteArray filename");
+		}
+
+		public static void AsByteArray(string variableName, TextWriter writer, long length, BinaryReader reader)
+		{
+			writer.WriteLine("private static byte[] {0} = ", variableName);
+			writer.Write("\t{");
+			for (int i = 0; i < length; i++)
+			{
+				if (i%15 == 0)
+				{
+					writer.WriteLine();
+					writer.Write("\t\t");
+				}
+				writer.Write("0x{0:X2}, ", reader.ReadByte());
+			}
+			writer.WriteLine();
+			writer.WriteLine("\t};");
+		}
+	}
+}
---
+++ Tests/when_creating_byte_array_source.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using AsByteArray;
+using Xunit;
+
+namespace Tests
+{
+	public class when_creating_byte_array_source
+	{
+		[Fact]
+		public void then_is_works()
+		{
+			var outMemoryStream = new MemoryStream();
+			var inMemoryStream = new MemoryStream(new byte[] {1, 2, 3, 4});
+			var streamWriter = new StreamWriter(outMemoryStream);
+			Program.AsByteArray("bytes", streamWriter, 4, new BinaryReader(inMemoryStream));
+			streamWriter.Flush();
+			var text = streamWriter.Encoding.GetString(outMemoryStream.ToArray());
+			Assert.Equal(@"private static byte[] bytes = 
+	{
+		0x01, 0x02, 0x03, 0x04, 
+	};
+", text);
+		}
+	}
+}
